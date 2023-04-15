2bff0ac1cc5bb771b3eab3287e319b2f549719d3
Brenley Dueck <brenleydueck@gmail.com>
2023-04-14 19:34:57 -0500

    Improve UI

--- src/components/EmojiMatchGame.tsx
+++ src/components/EmojiMatchGame.tsx
@@ -1,43 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type EmojiMatchGameProps = {
     startNewGame: () => void,
     completedBoard: string[][]
 };
 
-type CurrentGuess = {
+type Guess = {
     x: number;
     y: number;
 }
 
 function EmojiMatchGame({ startNewGame, completedBoard }: EmojiMatchGameProps) {
-    const [currentGuess, setCurrentGuess] = useState<CurrentGuess | null>(null);
     const [currentBoard, setCurrentBoard] = useState(completedBoard.map(row => row.map(_ => '')));
+
+    const [firstGuess, setFirstGuess] = useState<Guess | null>(null);
+    const [secondGuess, setSecondGuess] = useState<Guess | null>(null);
+
     const [numCorrect, setNumCorrect] = useState(0);
     const [numGuesses, setNumGuesses] = useState(0);
+    const [inProgress, setInProgress] = useState(false);
 
-    const handleGuess = ({ x, y }: CurrentGuess) => {
-        if (currentBoard[x][y]) {
-            return;
-        }
-
-        const nextBoard = [...currentBoard];
-        nextBoard[x][y] = completedBoard[x][y];
-
-        if (!currentGuess) {
-            setCurrentGuess({ x, y });
-        } else {
-            if (completedBoard[currentGuess.x][currentGuess.y] !== completedBoard[x][y]) {
-                nextBoard[x][y] = '';
-                nextBoard[currentGuess.x][currentGuess.y] = '';
+    useEffect(() => {
+        if (firstGuess && secondGuess) {
+            if (completedBoard[firstGuess.x][firstGuess.y] !== completedBoard[secondGuess.x][secondGuess.y]) {
+                setInProgress(true);
+                setTimeout(() => {
+                    const nextBoard = JSON.parse(JSON.stringify(currentBoard));
+                    nextBoard[firstGuess.x][firstGuess.y] = '';
+                    nextBoard[secondGuess.x][secondGuess.y] = '';
+                    setCurrentBoard(nextBoard);
+                    setInProgress(false);
+                }, 750);
             } else {
                 setNumCorrect(numCorrect + 1);
             }
-            setCurrentGuess(null);
+
+            setFirstGuess(null);
+            setSecondGuess(null);
             setNumGuesses(numGuesses + 1);
+
         }
 
-        setCurrentBoard(nextBoard)
+        const nextBoard = JSON.parse(JSON.stringify(currentBoard));
+        if (firstGuess) {
+            nextBoard[firstGuess.x][firstGuess.y] = completedBoard[firstGuess.x][firstGuess.y];
+        }
+        if (secondGuess) {
+            nextBoard[secondGuess.x][secondGuess.y] = completedBoard[secondGuess.x][secondGuess.y];
+        }
+
+        setCurrentBoard(nextBoard);
+
+    }, [firstGuess, secondGuess]);
+
+    const revealEmoji = ({ x, y }: Guess) => {
+        if (currentBoard[x][y] || inProgress) {
+            return;
+        }
+
+        if (!firstGuess) {
+            setFirstGuess({ x, y })
+        } else if (!secondGuess) {
+            setSecondGuess({ x, y })
+        }
     }
 
     return (
@@ -49,7 +74,7 @@ function EmojiMatchGame({ startNewGame, completedBoard }: EmojiMatchGameProps) {
                         <button
                             key={y}
                             className="cell"
-                            onClick={() => handleGuess({ x, y })}
+                            onClick={() => revealEmoji({ x, y })}
                         >
                             {emoji ? emoji : <div>&nbsp;</div>}
                         </button>
